e9e6b5455cd047cbc372857fbd98711c2b0aa2ce
estebanmaciel <estebanmaciel@gmail.com>
2022-01-09 10:26:22 -0300

    Primera NavBar tienda de libros

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar/Navbar"
+
+// function App(){
+//   return (
+    
+//     <div>
+//       {console.warn("Hola")}
+//       <h1 style={{color:"red"}}>Hola Mundo desde React {5+5}</h1>
+//     </div>
+//   )
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   return (
+     <div className="App">
+       <header className="App-header">
+         <Navbar />
+         <img src={logo} className="App-logo" alt="logo" />
+         {/* <p>
+           Editar <code>src/App.js</code> y guardarlo para recargar la página.
+         </p> */}
+         <a
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Tienda de libros
+         </a>
+       </header>
+     </div>
+   );
+ }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import "./Navbar.css"
+
+function Navbar(){
+    return (
+    <div >
+    <nav className="Navbar">
+        <ul>
+            <li><a href="#">Novelas</a></li>
+            <li><a href="#">Historia</a></li>
+            <li><a href="#">Nivel Inicial</a></li>
+            <li><a href="#">Educación primaria</a></li>
+            <li><a href="#">Educación secundaria</a></li>
+            <li><a href="#">Educación superior</a></li>
+            <li><a href="#">Sobre nosotros</a></li>
+        </ul>
+  </nav>
+  </div>)
+}
+
+export default Navbar
